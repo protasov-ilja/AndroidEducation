4ec57ab83e9cb7b4e1d47863384aad30eabdca4b
Ilya Protasov <ilya.protasov@omega-r.com>
2019-05-17 23:36:51 +0300

    added vectorimages support in gradle

--- EducationGoogle1/app/src/main/java/com/example/googleet11/MainActivity.kt
+++ EducationGoogle1/app/src/main/java/com/example/googleet11/MainActivity.kt
@@ -3,11 +3,14 @@ package com.example.googleet11
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var diceImage:ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,15 +18,29 @@ class MainActivity : AppCompatActivity() {
 
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.text = "Let's Roll"
+
         rollButton.setOnClickListener {
             Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
             rollDice()
         }
+
+        diceImage = findViewById(R.id.dice_image)
     }
 
     private fun rollDice() {
         val randomInt = Random().nextInt(6) + 1
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
         val resultText: TextView = findViewById(R.id.result_text)
+
+        diceImage.setImageResource(drawableResource)
         resultText.text = randomInt.toString()
     }
 }
